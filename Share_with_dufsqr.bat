a572bc932a24aa3312524606c83f22654bc1677c
natri23 <natri23@gmail.com>
2026-04-09 19:02:06 +0700

    Add files via upload

--- Share_with_dufsqr.bat
+++ Share_with_dufsqr.bat
@@ -6,5 +6,5 @@ powershell -NoProfile -ExecutionPolicy Bypass -Command ^
 "$ip = (Test-Connection -ComputerName $env:COMPUTERNAME -Count 1).IPV4Address.IPAddressToString; ^
 $url = \"http://${ip}:5000\"; ^
 try { Set-Clipboard -Value $url } catch { $url ^| clip }; ^
-$dufs = Start-Process dufs -ArgumentList ('-A -p 5000 \"' + '%target%' + '\"') -PassThru; ^
+$dufs = Start-Process dufs -ArgumentList ('--allow-upload --allow-search --allow-archive -p 5000 \"' + '%target%' + '\"') -PassThru; ^
 Start-Process 'chrome.exe' \"--new-window https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=$url\"; "
